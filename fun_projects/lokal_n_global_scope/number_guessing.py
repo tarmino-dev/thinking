b7b71f5d7ae3d099490ea39814bc2f1cf2c7b37e
Tarmino Dev <andrii.f313@gmail.com>
2025-01-29 19:27:23 +0100

    docstrings and refactoring

--- fun_projects/lokal_n_global_scope/number_guessing.py
+++ fun_projects/lokal_n_global_scope/number_guessing.py
@@ -1,39 +1,54 @@
 from art import logo
 from random import randint
+from collections import defaultdict
 
-print(logo)
+def get_difficulty():
+    """Asks user for the difficulty level"""
+    difficulty_mapping = defaultdict(lambda: 5, {"easy": 10, "hard": 5})
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    return difficulty_mapping[difficulty]
 
-number = randint(1, 100)
-print(f"DEBUG. number: {number}")
+def get_user_guess():
+    """Asks user for a guess."""
+    while True:
+        try:
+            return int(input(f"Make a guess: "))
+        except ValueError:
+            print("Please enter a valid number!")
 
-difficulty = input("""Welcome to the Number Guessing Game!
-I'm thinking of a number between 1 and 100.
-Choose a difficulty. Type 'easy' or 'hard': """)
+def play_game():
+    """Basic game logic."""
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    number = randint(1, 100)
+    attempts = get_difficulty()
+    print(f"DEBUG. number: {number}")
 
-difficulty_mapping = {"easy": 10, "hard": 5}
-attempts_number = difficulty_mapping[difficulty]
-
-def give_attempt(comparison_result, attempts):
-    attempts -= 1
-    if attempts > 0:
-        print(f"""Too {comparison_result}.\nGuess again.""")
-    return attempts
-
-while attempts_number > 0:
-    user_guess = int(input(f"""You have {attempts_number} attempts remaining to guess the number.\nMake a guess: """))
-    if number > user_guess:
-        attempts_number = give_attempt("low", attempts_number)
-    elif number < user_guess:
-        attempts_number = give_attempt("high", attempts_number)
-    else:
-        print(f"You got it! The answer was {number}.")
-        break
-else:
+    while attempts > 0:
+        print(f"You have {attempts} attempts remaining to guess the number.")
+        user_guess = get_user_guess()
+        if number > user_guess:
+            if attempts == 1:
+                print("Too low.")
+            else:
+                print(f"Too low.\nGuess again.")
+        elif number < user_guess:
+            if attempts == 1:
+                print("Too high.")
+            else:
+                print(f"Too high.\nGuess again.")
+        else:
+            print(f"You got it! The answer was {number}.")
+            return
+        attempts -= 1
     print("You've run out of guesses. Restart the game to play again.")
 
 
-print("-------------DEBUGGING-------------")
-print(f"DEBUG. number: {number}")
-print(f"DEBUG. difficulty: {difficulty}")
-print(f"DEBUG. attempts_number: {attempts_number}")
-print(f"DEBUG. user_guess: {user_guess}")
+    print("-------------DEBUGGING-------------")
+    print(f"DEBUG. number: {number}")
+    print(f"DEBUG. attempts: {attempts}")
+    print(f"DEBUG. user_guess: {user_guess}")
+
+if __name__ == "__main__":
+    play_game()
