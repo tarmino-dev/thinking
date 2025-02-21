8f90fdce8155dd1b5ff590b784d9dd5eb14ae600
Tarmino Dev <andrii.f313@gmail.com>
2025-02-21 20:39:16 +0100

    draw a dashed line

--- fun_projects/turtle_challenges/main.py
+++ fun_projects/turtle_challenges/main.py
@@ -1,9 +1,17 @@
 from turtle import Turtle, Screen
 
 bill = Turtle()
-bill.fd(100)
-for i in range(3):
-    bill.rt(90)
+for i in range(4):
+    print(bill.pos())
     bill.fd(100)
+    bill.rt(90)
+john = Turtle()
+john.up()
+john.setposition(-300, -200)
+for i in range(10):
+    john.down()
+    john.fd(25)
+    john.up()
+    john.fd(25)
 screen = Screen()
 screen.exitonclick()
