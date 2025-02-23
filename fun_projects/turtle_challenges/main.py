32f63d9535f007b0ec92a53fa3386a08bf1c3d25
Tarmino Dev <andrii.f313@gmail.com>
2025-02-23 16:10:41 +0100

    paint a 'dot' painting

--- fun_projects/turtle_challenges/main.py
+++ fun_projects/turtle_challenges/main.py
@@ -6,6 +6,7 @@ john = Turtle()
 jack = Turtle()
 burt = Turtle()
 nick = Turtle()
+gary = Turtle()
 
 
 def random_color():
@@ -34,6 +35,7 @@ def john_go():
 def jack_go():
     """Jack draws 8 polyhedrons with the number of edges from 3 to 10"""
     colormode(255)
+
     def draw_polyhedron(number_of_edges):
         jack.pencolor(random_color())
         for _ in range(number_of_edges):
@@ -58,17 +60,34 @@ def nick_go():
     """Nick imitates spirograph"""
     colormode(255)
     nick.speed("fastest")
+
     def draw_spirograph(size_of_gap):
         for _ in range(int(360 / size_of_gap)):
             nick.pencolor(random_color())
             nick.circle(100)
-            nick.setheading(nick.heading() + size_of_gap) # Get the current heading and turn it by X degrees
+            # Get the current heading and turn it by X degrees
+            nick.setheading(nick.heading() + size_of_gap)
     draw_spirograph(5)
 
 
+def gary_go():
+    """Gary paints a 'dot' painting"""
+    colormode(255)
+    gary.speed("fastest")
+    gary.penup()
+    gary.setposition(-300, -300)
+    for _ in range(10):
+        for _ in range(10):
+            gary.dot(20, random.choice(DOT_COLORS))
+            gary.forward(50)
+        gary.setposition(-300, gary.position()[1] + 50)
+
+
 TURTLES_FUNCTIONS = {"bill": bill_go, "john": john_go,
-                     "jack": jack_go, "burt": burt_go, "nick": nick_go}
+                     "jack": jack_go, "burt": burt_go, "nick": nick_go, "gary": gary_go}
 DIRECTIONS = [0, 90, 180, 270]
+DOT_COLORS = [(36, 95, 183), (236, 165, 79), (244, 223, 87), (215, 69, 105), (98, 197, 234), (250, 51, 22), (203, 70, 21), (240, 106, 143), (185, 47, 90), (143, 233, 216), (252, 136, 166), (165, 175, 233),
+              (66, 45, 13), (72, 205, 170), (83, 187, 100), (20, 156, 51), (24, 36, 86), (252, 220, 0), (164, 28, 8), (105, 39, 44), (250, 152, 2), (22, 151, 229), (108, 213, 249), (254, 12, 3), (38, 48, 98), (98, 96, 186)]
 
 
 def run_turtle(turtle):
@@ -77,12 +96,12 @@ def run_turtle(turtle):
 
 while True:
     user_choice = input(
-        "Choose your turtle (bill / john / jack / burt / nick) or type 'off' and close the window to exit: ")
+        "Choose your turtle (bill / john / jack / burt / nick / gary)\nor type 'off' and close the window to exit: ")
     if user_choice == "off":
         print("Exiting... Please close the canvas window.")
         break
     if user_choice not in TURTLES_FUNCTIONS:
-        print("Invalid input. Type bill / john / jack / burt / nick")
+        print("Invalid input. Type bill / john / jack / burt / nick / gary")
         continue
     resetscreen()
     run_turtle(user_choice)
