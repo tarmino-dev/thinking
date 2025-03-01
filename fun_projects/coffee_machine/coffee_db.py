6474165c8f269b3ff1455dc3b702b5798a5b43d4
Tarmino Dev <andrii.f313@gmail.com>
2025-03-01 21:09:02 +0100

    exception handling, logging improving

--- fun_projects/coffee_machine/coffee_db.py
+++ fun_projects/coffee_machine/coffee_db.py
@@ -13,15 +13,19 @@ class CoffeeMachineDB:
     def __init__(self):
         """Initialize the coffee machine with resources and profit, loading from the database."""
         logging.info("Connecting to the database...")
-        self.conn = pyodbc.connect(
-            f"DRIVER={config.DB_CONFIG['driver']};"
-            f"SERVER={config.DB_CONFIG['server']};"
-            f"DATABASE={config.DB_CONFIG['database']};"
-            f"UID={config.DB_CONFIG['username']};"
-            f"PWD={config.DB_CONFIG['password']}"
-        )
-        self.cursor = self.conn.cursor()
-        logging.info("Connection successful!")
+        try:
+            self.conn = pyodbc.connect(
+                f"DRIVER={config.DB_CONFIG['driver']};"
+                f"SERVER={config.DB_CONFIG['server']};"
+                f"DATABASE={config.DB_CONFIG['database']};"
+                f"UID={config.DB_CONFIG['username']};"
+                f"PWD={config.DB_CONFIG['password']}"
+            )
+            self.cursor = self.conn.cursor()
+            logging.info("Connection successful!")
+        except Exception as e:
+            print(f"Connection error: {e}")
+            logging.error("Connection error.")
         self.resources, self.profit = self.load_resources()
 
     def load_resources(self):
@@ -32,12 +36,17 @@ class CoffeeMachineDB:
             return {"water": [row.water_ml, 'ml'], "milk": [row.milk_ml, 'ml'], "coffee": [row.coffee_g, 'g']}, row.money
         return None
 
-    def save_resources(self):
-        """Saves the current resources and profit to the database."""
-        self.cursor.execute("""
-            UPDATE Resources
-            SET water_ml = ?, milk_ml = ?, coffee_g = ?, money = ?""", (self.resources["water"][0], self.resources["milk"][0], self.resources["coffee"][0], self.profit))
-        self.conn.commit()
+    def update_resources(self):
+        """Updates the database with the current resources and profit."""
+        try:
+            self.cursor.execute("""
+                UPDATE Resources
+                SET water_ml = ?, milk_ml = ?, coffee_g = ?, money = ?""", (self.resources["water"][0], self.resources["milk"][0], self.resources["coffee"][0], self.profit))
+            self.conn.commit()
+            logging.info("Database updated successfully.")
+        except Exception as e:
+            print(f"Error occured while updating the database: {e}")
+            logging.error("Error occured while updating the database.")
 
     def print_report(self):
         """Prints the current status of resources and profit."""
@@ -77,11 +86,11 @@ class CoffeeMachineDB:
                     print("Invalid input. Please enter a valid number.")
 
             self.resources[resource][0] += amount
-            self.save_resources()
             logging.info(
                 f"Refilled {amount}{self.resources[resource][1]} of {resource}")
             print(
                 f"{amount}{self.resources[resource][1]} of {resource} added successfully!")
+            self.update_resources()
 
     def is_enough_resources(self, order):
         """Checks if there are enough resources for the selected drink."""
@@ -115,22 +124,24 @@ class CoffeeMachineDB:
         order_cost = MENU[order]['cost']
         if order_cost > total_inserted:
             logging.warning(
-                f"Transaction failed: inserted ${total_inserted}, required ${order_cost}.")
+                f"Transaction failed: required ${order_cost}, inserted ${total_inserted}.")
             print(
                 f"Your drink costs ${order_cost}, but you have inserted only ${total_inserted}")
             print("Sorry, that's not enough money. Money refunded.")
             return False, 0
-        logging.info(f"Transaction successful: ${total_inserted} inserted.")
         change = round(total_inserted - order_cost, 2)
         if change > 0:
             print(f"Here is ${change} in change.")
+            logging.info(f"Transaction successful: required ${order_cost}, inserted ${total_inserted}, {change} returned as change")
+        else:
+            logging.info(f"Transaction successful: required ${order_cost}, inserted ${total_inserted}.")
         return True, order_cost
 
     def make_coffee(self, order):
         """Deducts the required ingredients and serves the coffee."""
         for ingredient, amount in MENU[order]["ingredients"].items():
             self.resources[ingredient][0] -= amount
-        self.save_resources()
+        self.update_resources()
         logging.info(f"Dispensed: {order}.")
         print(f"Here is your {order}. Enjoy!")
 
@@ -142,9 +153,9 @@ class CoffeeMachineDB:
             order = input(
                 "What would you like? (espresso/latte/cappuccino/report/refill/off): ").lower()
             if order == "off":
-                logging.info("Coffee machine turned off.")
                 print("Saving resources and shutting down...")
-                self.save_resources()
+                self.update_resources()
+                logging.info("Coffee machine turned off.")
                 print("The coffee machine is off. Bye!")
                 break
             if order == "report":
@@ -165,4 +176,4 @@ class CoffeeMachineDB:
             if enough_money:
                 self.make_coffee(order)
                 self.profit += Decimal(income)
-                self.save_resources()
+                self.update_resources()
