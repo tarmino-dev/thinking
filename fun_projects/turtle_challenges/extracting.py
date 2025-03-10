9644813ea8f216b358c4d16e5135390113d9cc61
Tarmino Dev <andrii.f313@gmail.com>
2025-03-10 22:54:15 +0100

    paint a 'dot' painting using extracted colors

--- fun_projects/turtle_challenges/extracting.py
+++ fun_projects/turtle_challenges/extracting.py
@@ -4,15 +4,11 @@ colors = colorgram.extract('image.jpg', 30)
 
 """Extracting colors from images. 3 ways of accessing fields of NamedTuple."""
 
-color_list1 = [] # for accessing fields by index
+color_list = [] # for accessing fields by index
 color_list2 = [] # for alternative way of accessing fields by keyname
 color_list3 = [] # for alternative way of accessing fields using getattr()
 
 for color in colors:
-    color_list1.append((color.rgb[0], color.rgb[1], color.rgb[2])) # by index
+    color_list.append((color.rgb[0], color.rgb[1], color.rgb[2])) # by index
     color_list2.append((color.rgb.r, color.rgb.g, color.rgb.b)) # by keyname
     color_list3.append((getattr(color.rgb, "r"), getattr(color.rgb, "g"), getattr(color.rgb, "b"))) # using getattr()
-
-print(color_list1)
-print(color_list2)
-print(color_list3)
