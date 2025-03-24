1929eb17d8635ea3aa6464d747e46dfa5a8102ab
Tarmino Dev <andrii.f313@gmail.com>
2025-03-24 12:34:06 +0100

    scoreboard

--- fun_projects/turtle_challenges/crossing_game/main.py
+++ fun_projects/turtle_challenges/crossing_game/main.py
@@ -4,8 +4,6 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-# Create a turtle player that starts at the bottom of the screen and listen for the "Up" keypress
-# to move the turtle north.
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -13,6 +11,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -21,19 +20,18 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
     car_manager.create_car()
     car_manager.move_cars()
-
     # Detect collision with car
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
-
+            scoreboard.game_over()
     # Detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
 
 screen.exitonclick()
