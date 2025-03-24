7199d0ef8723ed95324e4f4a9bd00eb6f8f1eca3
Tarmino Dev <andrii.f313@gmail.com>
2025-03-24 16:48:26 +0100

    snake: add highscore, reset snake and scoreboard instead of game over

--- fun_projects/turtle_challenges/snake_game/scoreboard.py
+++ fun_projects/turtle_challenges/snake_game/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -15,15 +16,16 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT,
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.highscore}", move=False, align=ALIGNMENT,
                    font=FONT)
 
-    def game_over(self):
-        self.goto(-50, 0)
-        self.write("GAME OVER", move=False, align=ALIGNMENT,
-                   font=FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
