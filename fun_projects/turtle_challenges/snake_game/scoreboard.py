a53d892da77d91bfef54556d79e48e11b1611d85
Tarmino Dev <andrii.f313@gmail.com>
2025-03-25 10:21:48 +0100

    snake: store highscore in a file

--- fun_projects/turtle_challenges/snake_game/scoreboard.py
+++ fun_projects/turtle_challenges/snake_game/scoreboard.py
@@ -5,10 +5,13 @@ FONT = ("Arial", 18, "normal")
 
 
 class Scoreboard(Turtle):
+
+    DATA_FILE = "data.txt"
+
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        self.highscore = self.read_highscore()
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -23,9 +26,19 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            self.save_highscore()
         self.score = 0
         self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
+
+    def read_highscore(self):
+        with open(self.DATA_FILE, mode="r") as data:
+            highscore = int(data.read())
+            return highscore
+
+    def save_highscore(self):
+        with open(self.DATA_FILE, mode="w") as data:
+            data.write(str(self.highscore))    
