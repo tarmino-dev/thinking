16fa2d45c3f93844fff2f3e24d021c2e7d5c763c
Tarmino Dev <andrii.f313@gmail.com>
2025-04-10 12:06:49 +0200

    tkinter: pomodoro: layout

---
+++ fun_projects/tkinter_basics/pomodoro/main.py
@@ -0,0 +1,56 @@
+import tkinter
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
+def timer_reset():
+    pass
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def timer_start():
+    pass
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Create a canvas (highlightthickness - border)
+canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+# Create an image object from the file
+tomato_img = tkinter.PhotoImage(file="tomato.png")
+# The center of the image (adjust if cropped), The image obgect
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white",
+                   font=(FONT_NAME, 35, "bold"))
+# Put the canvas on the window
+canvas.grid(column=1, row=1)
+
+title_label = tkinter.Label(text="Timer", bg=YELLOW,
+                            fg=GREEN, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+checkmark_label = tkinter.Label(text="✔", bg=YELLOW, fg=GREEN)
+checkmark_label.grid(column=1, row=3)
+
+start_button = tkinter.Button(
+    text="Start", highlightbackground=YELLOW, command=timer_start)
+start_button.grid(column=0, row=2)
+
+reset_button = tkinter.Button(
+    text="Reset", highlightbackground=YELLOW, command=timer_reset)
+reset_button.grid(column=2, row=2)
+
+window.mainloop()
