03abd4ffaaf1d4403ff037f18b0b32857cbab41a
Tarmino Dev <andrii.f313@gmail.com>
2025-05-03 12:16:55 +0200

    api: quizzler: complete transfer from console to gui

--- fun_projects/api/quizzler-app/ui.py
+++ fun_projects/api/quizzler-app/ui.py
@@ -1,10 +1,13 @@
 import tkinter
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    # Add a type hint to let Python know the type of passed object
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = tkinter.Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
@@ -16,17 +19,49 @@ class QuizInterface:
         self.canvas = tkinter.Canvas(
             bg="white", width=300, height=250, highlightthickness=0)
         self.question_text = self.canvas.create_text(
-            150, 125, text="QUESTION TEXT", font=("Arial", 20, "italic"), fill=THEME_COLOR)
+            150,
+            125,
+            width=280,  # To avoid going beyond the canvas
+            text="QUESTION TEXT",
+            font=("Arial", 20, "italic"),
+            fill=THEME_COLOR)
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         true_button_image = tkinter.PhotoImage(file="./images/true.png")
-        self.right_button = tkinter.Button(
-            image=true_button_image, highlightthickness=0)
-        self.right_button.grid(column=0, row=2)
+        self.true_button = tkinter.Button(
+            image=true_button_image, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(column=0, row=2)
 
         false_button_image = tkinter.PhotoImage(file="./images/false.png")
-        self.wrong_button = tkinter.Button(
-            image=false_button_image, highlightthickness=0)
-        self.wrong_button.grid(column=1, row=2)
+        self.false_button = tkinter.Button(
+            image=false_button_image, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(column=1, row=2)
+
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()  # Call the method of passed QuizBrain instance
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(
+                self.question_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_correct):
+        if is_correct is True:
+            self.canvas.config(bg="green")
+        elif is_correct is False:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
