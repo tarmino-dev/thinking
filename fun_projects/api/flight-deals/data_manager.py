e0b773bd7a8e17a79a77cd992b9902835d2ad9b0
Tarmino Dev <andrii.f313@gmail.com>
2025-05-28 10:19:26 +0200

    api: flight deals: config, .env and refactoring

--- fun_projects/api/flight-deals/data_manager.py
+++ fun_projects/api/flight-deals/data_manager.py
@@ -1,3 +1,26 @@
+import requests
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    """This class is responsible for talking to the Google Sheet."""
+
+    def __init__(self, endpoint, token):
+        self.endpoint = endpoint
+        self.headers = {"Authorization": f"Bearer {token}"}
+
+    def get_sheet(self):
+        response = requests.get(url=self.endpoint, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
+
+    def put_iata_code(self, row_id, iata_code):
+        sheet_inputs = {
+            "price": {
+                "iataCode": iata_code
+            }
+        }
+        response = requests.put(url=f"{self.endpoint}/{row_id}",
+                                json=sheet_inputs,
+                                headers=self.headers)
+        response.raise_for_status()
+        print(response.text)
--- fun_projects/api/flight-deals/flight_search.py
+++ fun_projects/api/flight-deals/flight_search.py
@@ -1,3 +1,41 @@
+import requests
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    """This class is responsible for talking to the Flight Search API."""
+
+    def __init__(self, token_endpoint, city_search_endpoint, api_key, api_secret):
+        self.token_endpoint = token_endpoint
+        self.city_search_endpoint = city_search_endpoint
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+    def get_token(self):
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        data = {
+            "grant_type": "client_credentials",
+            "client_id": self.api_key,
+            "client_secret": self.api_secret,
+        }
+        response = requests.post(
+            url=self.token_endpoint, headers=headers, data=data
+        )
+        response.raise_for_status()
+        token = response.json()['access_token']
+        return token
+
+    def make_headers(self):
+        headers = {
+            "authorization": f"Bearer {self.get_token()}"
+        }
+        return headers
+
+    def get_destination_code(self, city_name):
+        response = requests.get(url=self.city_search_endpoint,
+                                params={
+                                    "keyword": city_name},
+                                headers=self.make_headers())
+        response.raise_for_status()
+        response_json = response.json()
+        iata_code = response_json["data"][0]["iataCode"]
+        return iata_code
