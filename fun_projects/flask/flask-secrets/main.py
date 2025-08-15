27cc28d44dc27e11615620256699d3a989cdf34e
Tarmino Dev <andrii.f313@gmail.com>
2025-08-15 22:58:01 +0200

    flask: Flask WTForms: fields validation, receiving data from forms with WTForms

--- fun_projects/flask/flask-secrets/main.py
+++ fun_projects/flask/flask-secrets/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 class LoginForm(FlaskForm):
-    email = StringField(label="Email")
-    password = PasswordField(label="Password")
+    email = StringField(label="Email", validators=[DataRequired(), Email()])
+    password = PasswordField(label="Password", validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="Log In")
 
 
@@ -20,6 +21,11 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=login_form)
 
 
