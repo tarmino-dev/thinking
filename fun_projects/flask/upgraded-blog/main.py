954d2d8512b4f42a54ff41fd8cd1f384b0e2f761
Tarmino Dev <andrii.f313@gmail.com>
2025-09-24 10:28:09 +0200

    RESTful Blog: init

--- fun_projects/flask/upgraded-blog/main.py
+++ fun_projects/flask/upgraded-blog/main.py
@@ -1,28 +1,71 @@
-from flask import Flask, render_template
-import requests
-
-blog_url = "https://api.npoint.io/fbce267e92535cc0ad1b"
-response = requests.get(url=blog_url)
-all_posts = response.json()
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html", posts=all_posts)
+    # TODO: Query the database for all the posts. Convert the data to a python list.
+    posts = []
+    return render_template("index.html", all_posts=posts)
 
-@app.route('/about')
-def about_page():
+# TODO: Add a route so that you can click on individual posts.
+@app.route('/')
+def show_post(post_id):
+    # TODO: Retrieve a BlogPost from the database based on the post_id
+    requested_post = "Grab the post from your database"
+    return render_template("post.html", post=requested_post)
+
+
+# TODO: add_new_post() to create a new blog post
+
+# TODO: edit_post() to change an existing blog post
+
+# TODO: delete_post() to remove a blog post from the database
+
+# Below is the code from previous lessons. No changes needed.
+@app.route("/about")
+def about():
     return render_template("about.html")
 
-@app.route('/contact')
-def contact_page():
+
+@app.route("/contact")
+def contact():
     return render_template("contact.html")
 
-@app.route("/post/<int:index>")
-def show_post(index):
-    return render_template("post.html", post=all_posts[index - 1])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5003)
