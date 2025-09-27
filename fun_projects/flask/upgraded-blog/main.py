4d9f1bae12098cfa32ecac5670ee6d2078f00e54
Tarmino Dev <andrii.f313@gmail.com>
2025-09-27 12:45:33 +0200

    RESTful Blog: Be Able to DELETE Blog Posts

--- fun_projects/flask/upgraded-blog/main.py
+++ fun_projects/flask/upgraded-blog/main.py
@@ -102,7 +102,13 @@ def edit_post(post_id):
         return redirect(url_for("show_post", post_id=post.id))
     return render_template("make-post.html", form=form, is_edit=True)
 
-# TODO: delete_post() to remove a blog post from the database
+# delete_post() to remove a blog post from the database
+@app.route("/delete/<int:post_id>")
+def delete_post(post_id):
+    post_to_delete = db.session.get(entity=BlogPost, ident=post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
 
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")
