cd6d5df07787a1f0abe3e46a95fd6057520b2988
Tarmino Dev <andrii.f313@gmail.com>
2025-11-01 19:38:53 +0100

    Add flash notification (success/error) after sending a message in the Contact Form

--- fun_projects/flask/flask_lit_diary/app/routes/main.py
+++ fun_projects/flask/flask_lit_diary/app/routes/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, current_app
+from flask import Blueprint, render_template, redirect, url_for, current_app, flash
 from app.extensions import db
 from app.models.note import Note
 from app.forms.contact_forms import ContactForm
@@ -23,16 +23,25 @@ def contact():
     form = ContactForm()
     if form.validate_on_submit():
         email_msg = f"Subject: New message in the Literary Diary Conctact Form\n\nName: {form.name.data}\nEmail: {form.email.data}\nMessage: {form.message.data}"
-        send_email(email_msg)
+        success = send_email(email_msg)
+        if success:
+            flash("Your message has been sent successfully!", "success")
+        else:
+            flash("Something went wrong. Please try again later.", "error")
         return redirect(url_for("main.contact"))
     return render_template("contact.html", form=form)
 
 def send_email(message):
-    smtp_address = current_app.config["GMAIL_SMTP_ADDRESS"]
-    email = current_app.config["GMAIL_EMAIL"]
-    password = current_app.config["GMAIL_APP_PASSWORD"]
-    with smtplib.SMTP(smtp_address) as connection:
-        connection.starttls()
-        connection.login(user=email, password=password)
-        connection.sendmail(from_addr=email,
-                            to_addrs=email, msg=message)
+    try:
+        smtp_address = current_app.config["GMAIL_SMTP_ADDRESS"]
+        email = current_app.config["GMAIL_EMAIL"]
+        password = current_app.config["GMAIL_APP_PASSWORD"]
+        with smtplib.SMTP(smtp_address) as connection:
+            connection.starttls()
+            connection.login(user=email, password=password)
+            connection.sendmail(from_addr=email,
+                                to_addrs=email, msg=message)
+        return True
+    except Exception as e:
+        current_app.logger.error(f"Email sending failed: {e}") # Flask outputs this to Render Logs by default.
+        return False
