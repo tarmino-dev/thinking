f55659da409316b8cdd2cee18854e54b828e28aa
Tarmino Dev <andrii.f313@gmail.com>
2025-11-21 17:47:49 +0100

    Add basic command-line interface using Typer

--- fun_projects/rag/src/query_rag.py
+++ fun_projects/rag/src/query_rag.py
@@ -2,39 +2,43 @@ from langchain_community.vectorstores import FAISS
 from embedder import SentenceTransformerEmbeddings
 from langchain_core.prompts.chat import ChatPromptTemplate
 from langchain_ollama import OllamaLLM
+import typer
 
-embedder = SentenceTransformerEmbeddings()
+def main(query: str, k: int = 3, verbose: bool = False):
+    print("Your query:", query)
 
-loaded_faiss = FAISS.load_local("vectorstore", embedder, allow_dangerous_deserialization=True)
+    embedder = SentenceTransformerEmbeddings()
 
-print("FAISS index loaded\nDocs in FAISS:", len(loaded_faiss.docstore._dict))
+    loaded_faiss = FAISS.load_local("vectorstore", embedder, allow_dangerous_deserialization=True)
 
-user_query = input("Ask anything (e.g. 'How to fly to space?'): ")
+    print("FAISS index loaded\nDocs in FAISS:", len(loaded_faiss.docstore._dict))
 
-results_with_scores = loaded_faiss.similarity_search_with_score(query=user_query
-, k=2)
+    results_with_scores = loaded_faiss.similarity_search_with_score(query=query
+    , k=k)
 
-# Print results docs titles with their scores
-new_line = '\n'
-for doc, score in results_with_scores:
-    print(f"Score={score:.4f}  |  Text='{doc.page_content.split(new_line)[0]}'")
+    # Print results docs titles with their scores
+    new_line = '\n'
+    for doc, score in results_with_scores:
+        print(f"Score={score:.4f}  |  Text='{doc.page_content.split(new_line)[0]}'")
 
-# Pick the most relevant doc
-context = results_with_scores[0][0].page_content
+    # Pick the most relevant doc
+    context = results_with_scores[0][0].page_content
 
-# Prompt with placeholders
-prompt_template = ChatPromptTemplate.from_template(
-    "Context: " \
-    "{context}." \
-    "Request: {query}. Provide a helpful answer based ONLY on the context."
-    
-)
+    # Prompt with placeholders
+    prompt_template = ChatPromptTemplate.from_template(
+        "Context: " \
+        "{context}." \
+        "Request: {query}. Provide a helpful answer based ONLY on the context."
+    )
 
-prompt = prompt_template.format(context=context, query=user_query)
+    prompt = prompt_template.format(context=context, query=query)
 
-# Local model via Ollama
-llm = OllamaLLM(model="gpt-oss:20B")
+    # Local model via Ollama
+    llm = OllamaLLM(model="gpt-oss:20B")
 
-result = llm.invoke(prompt)
+    result = llm.invoke(prompt)
 
-print(result)
+    print(result)
+
+if __name__ == "__main__":
+    typer.run(main)
