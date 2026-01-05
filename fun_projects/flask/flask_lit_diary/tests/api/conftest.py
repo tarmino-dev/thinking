612daa62de12adfe011fc2fbc6fef60bf35c06e8
Tarmino Dev <andrii.f313@gmail.com>
2026-01-05 12:08:58 +0100

    Create structure and base for API tests

---
+++ fun_projects/flask/flask_lit_diary/tests/api/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from app import create_app
+from app.extensions import db
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config.update(
+        TESTING=True,
+        SQLALCHEMY_DATABASE_URI="sqlite:///:memory:",
+        WTF_CSRF_ENABLED=False,
+        LOGIN_DISABLED=False,
+    )
+
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
