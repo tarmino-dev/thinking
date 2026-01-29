8bcc32a342a476dc12ce73782ad10413bd741853
Tarmino Dev <andrii.f313@gmail.com>
2026-01-29 09:20:33 +0100

    Create DB connection

---
+++ fun_projects/etl_vacancies/etl/db/connection.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5432/etl"
+)
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
