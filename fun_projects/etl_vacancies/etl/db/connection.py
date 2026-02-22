41eb89b12bb617e94e0a6d9367f5fa5a8a31e2ee
Tarmino Dev <andrii.f313@gmail.com>
2026-02-22 13:18:21 +0100

    Move database and Airflow credentials to .env configuration

--- fun_projects/etl_vacancies/etl/db/connection.py
+++ fun_projects/etl_vacancies/etl/db/connection.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:postgres@postgres:5432/etl"
+DATABASE_URL = (
+    f"postgresql://{os.environ['POSTGRES_USER']}:"
+    f"{os.environ['POSTGRES_PASSWORD']}@postgres:5432/"
+    f"{os.environ['POSTGRES_DB']}"
 )
 
 engine = create_engine(DATABASE_URL)
