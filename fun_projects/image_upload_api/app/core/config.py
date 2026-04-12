92a529523360855320df49967645537293a957aa
Tarmino Dev <andrii.f313@gmail.com>
2026-04-12 23:06:26 +0200

    Update config with AWS environment variables; Implement saving file to S3 storage

--- fun_projects/image_upload_api/app/core/config.py
+++ fun_projects/image_upload_api/app/core/config.py
@@ -8,5 +8,9 @@ class Settings:
 
     STORAGE_TYPE: str = os.getenv("STORAGE_TYPE", "local")
     UPLOAD_DIR: str = os.getenv("UPLOAD_DIR", "uploads")
+    AWS_ACCESS_KEY_ID: str = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY: str = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_BUCKET: str = os.getenv("AWS_S3_BUCKET")
+    AWS_REGION: str = os.getenv("AWS_REGION", "eu-central-1")
 
 settings = Settings()
--- fun_projects/image_upload_api/app/services/storage.py
+++ fun_projects/image_upload_api/app/services/storage.py
@@ -1,13 +1,36 @@
 import os
+import boto3
+import uuid
 from app.core.config import settings
 
-UPLOAD_DIR = settings.UPLOAD_DIR
-
 def save_file(file):
-    if settings.STORAGE_TYPE == "local":
-        return save_local(file)
+    if settings.STORAGE_TYPE == "s3":
+        return save_s3(file)
     else:
-        raise NotImplementedError("S3 not implemented yet")
+        return save_local(file)
+
+
+def save_s3(file):
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_REGION,
+    )
+
+    file_extension = file.filename.split(".")[-1]
+    unique_name = f"{uuid.uuid4()}.{file_extension}"
+
+    s3.upload_fileobj(
+        file.file,
+        settings.AWS_S3_BUCKET,
+        unique_name,
+        ExtraArgs={"ContentType": file.content_type}
+    )
+
+    file_url = f"https://{settings.AWS_S3_BUCKET}.s3.{settings.AWS_REGION}.amazonaws.com/{unique_name}"
+
+    return file_url
 
 
 def save_local(file):
