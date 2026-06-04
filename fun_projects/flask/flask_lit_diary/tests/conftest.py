d3686a0bacbbd57b3881cb4d299052edff185a4f
Tarmino Dev <andrii.f313@gmail.com>
2026-06-05 01:57:14 +0200

    fix: set SECRET_KEY in root test conftest to fix CI session errors

--- fun_projects/flask/flask_lit_diary/tests/conftest.py
+++ fun_projects/flask/flask_lit_diary/tests/conftest.py
@@ -8,6 +8,7 @@ def app():
         "TESTING": True,
         "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
         "WTF_CSRF_ENABLED": False,
+        "SECRET_KEY": "test-secret-key",
     })
 
     with app.app_context():
