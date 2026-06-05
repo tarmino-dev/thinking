9e8f4989fe160810388a443175886ebcb32ebf1f
Tarmino Dev <andrii.f313@gmail.com>
2026-06-06 01:55:10 +0200

    feat: harden session cookie flags in config

--- fun_projects/flask/flask_lit_diary/config.py
+++ fun_projects/flask/flask_lit_diary/config.py
@@ -2,6 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('FLASK_KEY')
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Lax'
+    SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE', 'false').lower() == 'true'
     GMAIL_EMAIL = os.environ.get('GMAIL_EMAIL')
     SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 
